be568f7b1987589d1e9bd10b5d0e5c589a950c8b
Iuliia <ytaratynova@gmail.com>
2022-10-12 19:25:08 +0300

    IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+    }
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
